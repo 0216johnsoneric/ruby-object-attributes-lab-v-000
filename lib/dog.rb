55cbd194db9d091f7b4b23f8c42fab9b9070575f
Eric Johnson <0216johnsoneric@gmail.com>
2017-11-05 02:00:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,26 +15,26 @@
 # puts lassie.name
 
 # class Dog
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def name
 #     @name
 #   end
 # end
-# 
+#
 # fido = Dog.new
 # fido.name = "fido"
-# 
+#
 # fido.name
 
 class Dog
   def name=(dog_name)
     this_dogs_name = dog_name
   end
- 
+
   def name
     this_dogs_name
   end
